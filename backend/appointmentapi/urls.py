2d0ee38f1658cf5f70032a49095b9c4a8da06f7a
Marc Deloso <202080169@psu.palawan.edu.ph>
2025-11-25 13:08:08 +0800

    profile 1

--- backend/appointmentapi/urls.py
+++ backend/appointmentapi/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('api/doctors/', include('appointmentapp.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
