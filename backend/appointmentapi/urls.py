2fa7e1bedb90c05cb02d69a1e50fbf74bee8b829
Marc Deloso <202080169@psu.palawan.edu.ph>
2026-01-14 06:12:52 +0800

    fix routing for patient login 5

--- backend/appointmentapi/urls.py
+++ backend/appointmentapi/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/doctors', include('appointmentapp.urls')),
+    path('api/doctors/', include('appointmentapp.urls')),
     path('api/patients/', include('appointmentapp.urls')),
 
 ]
