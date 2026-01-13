5d1393d325f69e717ebc7164013797ee613b1b2a
Marc Deloso <202080169@psu.palawan.edu.ph>
2026-01-14 04:13:45 +0800

    fix Protected Route 2

--- backend/appointmentapi/settings.py
+++ backend/appointmentapi/settings.py
@@ -48,8 +48,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL =
   window.location.hostname === "localhost"
     ? "http://127.0.0.1:8000/api/"
-    : "https://medconnect-app-ofag.onrender.com/api/";
+    : "https://medconnect-app-aee6.onrender.com/api/";
 
 const api = axios.create({
   baseURL: BASE_URL,
